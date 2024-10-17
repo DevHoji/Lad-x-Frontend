447982c0c538d6c782e72d0bcf61516437e4122e
yabselam <yabsirahoji@gmail.com>
2024-10-17 11:57:17 -0700

    typo error resolved

--- frontend/src/components/FormLayOut.jsx
+++ frontend/src/components/FormLayOut.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-import plane from "../assets/Plane.jpg";
+import plane from "../assets/plane.jpg";
 const FormLayOut = ({ children }) => {
   return (
     <Box
